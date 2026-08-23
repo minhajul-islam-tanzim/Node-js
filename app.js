8509d67c87b432d4b528933de6df8b92cc2d7263
tanzimminhaj777 <tanzimminhaj@gmail.com>
2026-08-23 11:23:50 +0600

    requist roution (event loop)

--- app.js
+++ app.js
@@ -4,11 +4,20 @@ const http = require('http')
 const server = http.createServer((req, res) => {
     console.log(req.headers, req.method, req.url)
    res.setHeader('content-type', 'text/html')
-  res.write('<html>')
-  res.write('<title>hello</title>')
-  res.write('<body>Hiiiii</body>')
-  res.write('</html>')
-  res.end()
+
+   if(req.url === '/'){
+
+       res.write('<html>')
+       res.write('<title>hello</title>')
+       res.write('<body>Home</body>')
+       res.write('</html>')
+      return res.end()
+   }
+         res.write('<html>')
+       res.write('<title>hello</title>')
+       res.write('<body>Others page</body>')
+       res.write('</html>')
+       res.end()
 
 
 }
