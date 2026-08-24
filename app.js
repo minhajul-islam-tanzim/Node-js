ee03a2e99e36a485284453ef7ada5fac6cef9919
tanzimminhaj777 <tanzimminhaj@gmail.com>
2026-08-24 11:54:59 +0600

    redirect requist

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 
 const http = require('http')
+const fs = require('fs')
 
 const server = http.createServer((req, res) => {
     console.log(req.headers, req.method, req.url)
    res.setHeader('content-type', 'text/html')
 
+
+    
    if(req.url === '/'){
 
        res.write('<html>')
        res.write('<title>hello</title>')
        res.write('<body><h1>Enter your details</h1>')
 
-       res.write('<form action="/submit-details method="POST"">')
-       res.write('<input type="text" name="username" placeholder="Enter Name""><br>')
+
+res.write('<form action="/submit-details" method="POST">')
+res.write('<input type="text" name="username" placeholder="Enter Name"><br>')
 
        res.write('<input type="radio" name="gender" id="male" value="male">')
        res.write('<label for="male">Male</label>')
@@ -25,7 +29,14 @@ const server = http.createServer((req, res) => {
         res.write('</body>')
        res.write('</html>')
       return res.end()
-   }
+
+   }else if(req.url.toLowerCase() === "/submit-details" && req.method == "POST"){ 
+    fs.writeFileSync('max.txt', 'complate')
+    res.statusCode = 302;
+    res.setHeader('Location', '/')
+   }   
+
+
          res.write('<html>')
        res.write('<title>hello</title>')
        res.write('<body>Others page</body>')
