bac49842a2e86c7fc6882ae96935a6e674cf3d93
tanzimminhaj777 <tanzimminhaj@gmail.com>
2026-09-09 11:35:54 +0600

    debuging

--- chapter-7-npm-tools/app.js
+++ chapter-7-npm-tools/app.js
@@ -1,10 +1,11 @@
 const http = require("http");
+const serverHandle = require("./user")
 
-const server = http.createServer((req, res) => {
-  console.log(req);
-});
+const server = http.createServer(serverHandle);
 
-const PORT = 3001;
+
+const PORT = 3002;
 server.listen(PORT, () => {
   console.log(`server is running in http://localhost:${PORT}`);
 });
+ 
