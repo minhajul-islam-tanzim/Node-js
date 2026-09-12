a2be14bbbb0ae916e933a56bedcda5f543d45342
tanzimminhaj777 <tanzimminhaj@gmail.com>
2026-09-12 19:42:23 +0600

    complated practice sets

--- Practice-sets/express-contact-us/app.js
+++ Practice-sets/express-contact-us/app.js
@@ -11,12 +11,36 @@ app.use((req, res, next) => {
 
 app.use((req, res, next) => {
     console.log("second dummy middleware", req.url, req.method)
+    next()
 })
 
-app.use((req, res, next) => {
-    console.log("Third middleware", req.url, req.method)
-    res.send("<h1>Welcome to complate coading</h1>")
+// app.use((req, res, next) => {
+//     console.log("Third middleware", req.url, req.method)
+//     res.send(`<h1>Welcome to complate coading</h1>`)
+// })
+
+app.get("/",(req, res, next) => {
+    console.log("second dummy middleware", req.url, req.method)
+     res.send(`<h1>Welcome to complate coading</h1>`)
 })
+
+app.get("/contact-us",(req, res, next) => {
+    console.log("contact us page ", req.url, req.method)
+     res.send(`
+        <h1>Please give me your details</h1>
+        <form action="/contact-us" method="POST">
+        <input type="text" name="name" placeholder="Enter your name" />
+        <input type="email" name="email" placeholder="Enter your Email" />
+        <input type="submit" value="click"/>    
+        </form>
+        `)
+})
+
+app.post("/contact-us",(req, res, next) => {
+    console.log("handling contact us for POST", req.url, req.method)
+     res.send(`<h1>Thanks fpr giving Info</h1>`)
+})
+
 
 
 
