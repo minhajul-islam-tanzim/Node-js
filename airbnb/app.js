16f57e727f9f89f45e88da169cc0afe41779fdab
tanzimminhaj777 <tanzimminhaj@gmail.com>
2026-09-13 16:46:47 +0600

    router learn done

--- airbnb/app.js
+++ airbnb/app.js
@@ -1,11 +1,27 @@
+// external module 
 const express = require("express");
+
+
+// local module 
+const userRouter = require("./routes/userRouter")
+const hostRouter = require("./routes/hostRouter");
+
+
 
 const app = express()
 
+
+
 app.use((req, res, next) => {
-    console.log("first", req.url, req.method);
-    res.send("<h1>Welcome to airbnb</h1>")
+    console.log("first", req.url, req.method)
+    next()
 })
+
+
+app.use(express.urlencoded())
+app.use(userRouter)
+app.use(hostRouter)
+
 
 
 
---
+++ airbnb/routes/hostRouter.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const hostRouter = express.Router();
+
+
+hostRouter.get("/host/add-home",(req, res, next) => {
+    console.log("first", req.url, req.method);
+    res.send(`<h1>Register your home here:</h1>
+        <form action = "/host/add-home" method="POST">
+            <input type="text" name="houseName" placeholder="Enter your house name""/>
+            <input type="submit"/>
+        </form>
+        `)
+})
+
+
+hostRouter.post("/host/add-home",(req, res, next) => {
+    console.log("first", req.url, req.method, req.body);
+    res.send(`<h1>${req.body.houseName + " " }Register succesfully</h1>
+        <a href="/">Go to home</a>
+        `)
+})
+
+module.exports = hostRouter;
